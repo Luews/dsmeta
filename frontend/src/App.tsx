f16c56dac9457c6dabb6ff0436b0d250985909c8
Lucas Cristiano <lucasdasilvacristiano@gmail.com>
2022-11-17 20:13:02 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import NotificationButton from "./components/notification_button"
+
 function App() {
-  return(
+  return (
+    <>
     <h1>Olá mundo </h1>
+    <NotificationButton />
+    <NotificationButton />
+    
+    </>
   )
- 
+
 }
 
 export default App
